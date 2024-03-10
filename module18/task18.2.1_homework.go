b737ecceb970491ed852d5e8166ba7dc611b69fc
Nick <mojiotok167@gmail.com>
2024-03-10 19:41:50 +0300

    update module 18

--- module18/task18.2.1_homework.go
+++ module18/task18.2.1_homework.go
@@ -5,9 +5,12 @@ import (
 	"sync"
 )
 
-const COUNT_ROUTINES = 5
-const PRINT_TIMES = 10
+const (
+	COUNT_ROUTINES = 5
+	PRINT_TIMES    = 10
+)
 
+// PrintNumber prints serial number of go the goroutine in which it is called.
 func printNumber(routine int) {
 	for i := 0; i < PRINT_TIMES; i++ {
 		fmt.Println(routine)
@@ -15,14 +18,21 @@ func printNumber(routine int) {
 }
 
 func main() {
+
+	//Create WaitGroup.
 	var wg sync.WaitGroup
 
+	//Add 5 goroutine in wait group. Inner atomic count is equal COUNT_ROUTINES.
 	wg.Add(COUNT_ROUTINES)
 	for routine := 1; routine <= COUNT_ROUTINES; routine++ {
+		//Create goroutine
 		go func(routine int) {
 			printNumber(routine)
+			//This method sends signal that the one of the goroutine finished. Inner atomic count decrement.
 			wg.Done()
 		}(routine)
 	}
+
+	//Wait all goroutine in group.
 	wg.Wait()
 }
