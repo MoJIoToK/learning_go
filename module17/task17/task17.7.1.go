111e66f61462ffe86f80b916a160f078d9ad7cea
Nick <mojiotok167@gmail.com>
2024-03-12 00:22:27 +0300

    some fix in task 17.7.1

--- module17/task17/task17.7.1.go
+++ module17/task17/task17.7.1.go
@@ -34,7 +34,7 @@ func main() {
 	fmt.Println("Укажите количество горутин: ")
 	_, err := fmt.Scanln(&amountOfThreads)
 	if err != nil {
-		log.Fatalln("Неверное значение")
+		log.Fatalln("Неверное значение!")
 	}
 	if amountOfThreads < 1 {
 		log.Fatalln("Количество горутин не может быть меньше 1")
@@ -44,7 +44,7 @@ func main() {
 	fmt.Println("Укажите максимальное значение счётчика: ")
 	_, err = fmt.Scanln(&maxValue)
 	if err != nil {
-		log.Fatalln("Неверное значение")
+		log.Fatalln("Неверное значение счётчика!")
 	}
 	if maxValue < 1 {
 		log.Fatalln("Максимальное значение счтчика не может быть меньше 1")
